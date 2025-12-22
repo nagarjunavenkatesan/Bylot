7e3256c1c6f7d69eed9e21bff000efae1784b4a2
Nagarjuna Venkatesan <nagarjunavenkatesan@gmail.com>
2025-12-22 20:26:52 +0530

    Configure GitHub Pages deployment

--- bylot/vite.config.js
+++ bylot/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Bylot/",
   server: {
     host: true, // Calls listen on all local IPs
     port: 5173,
